1413d7e27a1bcf5a709f428372772ff7d73d4aeb
Patricia Liu <patricialiu1168@gmail.com>
2020-09-03 10:27:05 -0400

    fix pictures

--- footer.js
+++ footer.js
@@ -1,7 +1,7 @@
 document.write('\
 \
 <a href="https://github.com/PatLiu1168" target="_blank">\
-<img src="Pictures/github.png" alt="Octocat" style="width:20px; height:20px; margin: 10px;">\
+<img src="Pictures/Github.png" alt="Octocat" style="width:20px; height:20px; margin: 10px;">\
 </a>\
 \
 <a href="https://www.linkedin.com/in/patricia-liu-a4589b153/" target="_blank">\
