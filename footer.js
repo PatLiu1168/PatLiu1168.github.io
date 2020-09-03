c28f18e55c318d02d0f5f2adec361e77355a5acd
Patricia Liu <patricialiu1168@gmail.com>
2020-09-03 10:31:59 -0400

    commit

--- footer.js
+++ footer.js
@@ -1,11 +1,11 @@
 document.write('\
 \
 <a href="https://github.com/PatLiu1168" target="_blank">\
-<img src="Pictures/Github.png" alt="Octocat" style="width:20px; height:20px; margin: 10px;">\
+<img src="Pictures/Github.png" alt="Octocat" style="width:25px; height:25px; margin: 10px;">\
 </a>\
 \
 <a href="https://www.linkedin.com/in/patricia-liu-a4589b153/" target="_blank">\
-<img src="Pictures/LinkedIn.png" alt="Squares are IN" style="width:20px; height:20px; margin: 10px;">\
+<img src="Pictures/LinkedIn.png" alt="LinkedIn" style="width:20px; height:20px; margin: 10px;">\
 </a>\
 \
 <a href="mailto:p99liu@uwaterloo.ca" target="_blank">\
